8776e78d44b0353d8ccd85c0569d043daaf8fab7
IvoryCandy <deovant@gmail.com>
2018-03-11 18:37:31 -0400

    remove unused code

--- CS3251_Project1/src/edu/gatech/cc/cs3251/ateichner3/edu/gatech/cc/cs3251/ateichner3/project1/rpnclientUDP.java
+++
@@ -1,5 +0,0 @@
-package edu.gatech.cc.cs3251.ateichner3.edu.gatech.cc.cs3251.ateichner3.project1;
-
-
-public class rpnclientUDP {
-}
--- CS3251_Project1/src/edu/gatech/cc/cs3251/ateichner3/edu/gatech/cc/cs3251/ateichner3/project1/rpnserverUDP.java
+++
@@ -1,4 +0,0 @@
-package edu.gatech.cc.cs3251.ateichner3.edu.gatech.cc.cs3251.ateichner3.project1;
-
-public class rpnserverUDP {
-}
