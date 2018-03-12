ed5d55f95e30773f8e40a600ea66dccda777242a
IvoryCandy <deovant@gmail.com>
2018-03-12 18:43:12 -0400

    UPDATE: change interface config

--- node.java
+++ node.java
@@ -1,5 +1,3 @@
-import a.d.S;
-
 import java.net.*;
 import java.io.*;
 import java.util.concurrent.ExecutorService;
@@ -15,9 +13,9 @@ public class node {
     private static int NUM_RINGO;
     static int NUM_ACTIVE_RINGO;
     static int PACKET_TRANSITTION_NUMBER;
-    static node[] KNOWN_RINGO_LIST;
+    static ArrayList<String[]> KNOWN_RINGO_LIST = new ArrayList<>();
     static int[][] RTT;
-    static byte[] IOQUEUE;
+    static Queue<byte[]> IO_QUEUE = new ArrayDeque<>();
 
     // status indicator
     private static Sender sender;
@@ -26,34 +24,48 @@ public class node {
     private static int PORT_NUM;
 
     public static void main(String[] args) {
-        if (args[0].equals("ringo") && args.length == 6) {
-            flag = args[1];
-            PORT_NUM = Integer.parseInt(args[2]);
-            String poc_name = args[3];
-            int poc_port = Integer.parseInt(args[4]);
-            NUM_RINGO = Integer.parseInt(args[5]);
+        final ExecutorService service = Executors.newCachedThreadPool();
 
-            final ExecutorService service = Executors.newCachedThreadPool();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String[] in = scanner.nextLine().trim().split(" ");
 
-            while (true) {
-                service.submit(new Sender(poc_name, poc_port, IOQUEUE));
+            if (in.length == 6 && in[0].equals("ringo")) {
+                flag = in[1];
+                PORT_NUM = Integer.parseInt(in[2]);
+                String poc_name = in[3];
+                String poc_port_str = in[4];
+                int poc_port = Integer.parseInt(poc_port_str);
+                add_poc(poc_name, poc_port_str);
+                NUM_RINGO = Integer.parseInt(in[5]);
+
+            } else if (in.length == 2 && in[0].equals("offline")) {
+                try {
+                    Thread.sleep(Integer.parseInt(in[1]));
+                } catch (InterruptedException e) {
+                    System.out.println("node offline failed");
+                }
+
+            } else {
+                System.out.println("invalid command, please try again !");
             }
-        } else if ((args[0].equals("offline") && args.length == 2)) {
-            
-        } else {
-            System.out.println("invalid command, please try again !");
+
+            scanner.close();
         }
     }
 
+    /**
+     * the private sender class
+     *
+     * in charge of sending data
+     */
     static class Sender implements Runnable{
         String OUT_IP;
         int OUT_PORT;
-        byte[] data;
 
-        Sender(String out_ip, int out_port, byte[] data) {
+        Sender(String out_ip, int out_port) {
             this.OUT_IP = out_ip;
             this.OUT_PORT = out_port;
-            this.data = data;
         }
 
         public void run() {
@@ -64,18 +76,20 @@ public class node {
                 InetAddress IPF = InetAddress.getByName(OUT_IP);
 
                 // send data
-                for (int i = 0; i < 4; i++) {
-                    byte[] out_data = Arrays.copyOfRange(data, 2500 * i, 2500 * (i + 1));
-                    DatagramPacket sendPkt = new DatagramPacket(out_data, 2500, IPF, OUT_PORT);
-                    socket.send(sendPkt);
+                while (true) {
+                    if (IO_QUEUE.size() != 0) {
+                        byte[] out_data = IO_QUEUE.poll();
+                        DatagramPacket sendPkt = new DatagramPacket(out_data, 2500, IPF, OUT_PORT);
+                        socket.send(sendPkt);
+                    }
                 }
+
             } catch (SocketException e) {
                 System.out.println("initializing socket failed");
             } catch (UnknownHostException e) {
                 System.out.println("cannot solve the destination IP address");
             } catch (IOException e) {
                 System.out.println("sending data failed");
-
             }
         }
     }
@@ -87,6 +101,34 @@ public class node {
 
         public void run() {
             // TODO
+        }
+    }
+
+    static class Manager implements Runnable {
+        Manager() {
+
+        }
+
+        public void run() {
+
+        }
+    }
+
+
+    /**
+     * add poc to known list if possible
+     * @param poc_name the IP address or DNS name of poc
+     * @param poc_port the port number of poc
+     */
+    private static void add_poc(String poc_name, String poc_port) {
+        for (int i = 0; i < KNOWN_RINGO_LIST.size(); i++) {
+            if (KNOWN_RINGO_LIST.get(i)[0].equals(poc_name)) {
+                System.out.println(poc_name + " is already in known_list");
+                break;
+            } else {
+                KNOWN_RINGO_LIST.add(new String[] {poc_name, poc_port});
+            }
+
         }
     }
 
